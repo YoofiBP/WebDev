44242c94fc66279e6a2cb36f240a6a26cf80d661
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2019-03-31 01:05:30 +0000

    Learn to use snippets

--- Backend Development/todolist-v1/app.js
+++ Backend Development/todolist-v1/app.js
@@ -5,15 +5,30 @@ const bodyParser = require('body-parser');
 const port = 3000;
 const app = express();
 
+var newItems = [];
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.get("/",function(req,res){
+  var option = {
+    weekday:"long",
+    day: "2-digit",
+    month:"long",
+    year:"numeric"
+  };
 
   var today = new Date();
-  console.log(today);
-  res.send("working");
+  var date = today.toLocaleDateString("en-US", option);
+  res.render("list",{fulldate:date, newListItem:newItems});
 })
+
+app.post("/", function(req,res){
+  newItem = req.body.newItem;
+  newItems.push(newItem)
+  res.redirect("/");
+})
+
 app.listen(port, function(){
   console.log("Server running on port "+port);
 })
