a9987903a98781b3b394a6607053e88cb6713ec7
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2019-04-09 21:22:19 +0000

    Mongo finished

--- Backend Development/FruitsProject/app.js
+++ Backend Development/FruitsProject/app.js
@@ -4,16 +4,60 @@ const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost:27017/shopDB", {useNewUrlParser: true});
 
-const UserSchema = new mongoose.Schema({
-  name: String,
-  age: Number
+const CourseSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: [true, 'Every course has a name']
+  },
+  code:{
+    type: String,
+    required: [true, 'Every course has a code']
+  },
+  teacher: String
 });
 
-const User = mongoose.model("User",UserSchema);
-
-const user =  new User({
-  name: "Selasi",
-  age: 7
+const StudentSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: [true, 'Every student has a name']
+  },
+  number: {
+    type: Number,
+    required: [true, 'Every student has a number']
+  },
+  courses: [CourseSchema]
 });
 
-user.save();
+const Course = mongoose.model("Course", CourseSchema);
+const Student = mongoose.model('Student', StudentSchema);
+
+const course = new Course({
+  name: "ISS",
+  code: "ASH 427",
+  teacher: "Jackson"
+});
+
+//course.save();
+
+const student = new Student({
+  name: "Yoofi Brown-Pobee",
+  number: 0248506381,
+  courses: [course]
+});
+
+//student.save();
+/*Student.updateOne({name: "Yoofi Brown-Pobee"}, {$push: {courses: course}}, function(err){
+  if(err){
+    console.log(err);
+  }else{
+    console.log("Success!");
+  }
+});*/
+
+Course.updateOne({_id: "5cad0b705c26fa666183f286"}, {teacher: "Isaac"}, function(err){
+  if(err){
+    console.log(err);
+  }else{
+    console.log("Success!");
+  }
+});
