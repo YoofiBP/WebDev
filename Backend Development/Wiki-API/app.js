5a0b4a620f4185a241455985b15857cdbb112722
Yoofi96 <joseph.brown-pobee@ashesi.edu.gh>
2019-04-21 04:27:21 +0000

    Authentication

--- Backend Development/Wiki-API/app.js
+++ Backend Development/Wiki-API/app.js
@@ -9,6 +9,21 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set('view engine','ejs');
 
-app.listen(3000, function(){
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
+
+const ArticleSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Article = mongoose.model("Article",ArticleSchema);
+
+app.get("/articles", function(req,res){
+  Article.find(function(err, results){
+    res.send(results);
+  });
+});
+
+app.listen(3002, function(){
   console.log("Server up and running on port 3000");
 });
