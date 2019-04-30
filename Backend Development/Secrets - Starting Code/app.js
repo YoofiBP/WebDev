ab45368ee5221d37e7a2cf023bb37ea97697281a
Yoofi96 <joseph.brown-pobee@ashesi.edu.gh>
2019-04-30 18:49:44 +0000

    Learnt to use Environmnet Variables

--- Backend Development/Secrets - Starting Code/app.js
+++ Backend Development/Secrets - Starting Code/app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
@@ -14,7 +15,7 @@ const UserSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "Thisisasecret";
+const secret = process.env.SECRET;
 
 UserSchema.plugin(encrypt, {secret: secret, encryptedFields:['password']});
 
