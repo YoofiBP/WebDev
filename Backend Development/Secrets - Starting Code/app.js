eb5a4f458c6f93e33949713f194dec1d88a7adda
Yoofi96 <joseph.brown-pobee@ashesi.edu.gh>
2019-05-01 20:20:00 +0000

    Remake of 40graph

--- Backend Development/Secrets - Starting Code/app.js
+++ Backend Development/Secrets - Starting Code/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -14,10 +14,6 @@ const UserSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-const secret = process.env.SECRET;
-
-UserSchema.plugin(encrypt, {secret: secret, encryptedFields:['password']});
 
 const User = new mongoose.model("User", UserSchema);
 
@@ -35,7 +31,7 @@ app.get("/login", function(req,res){
 
 app.post("/login", function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email:username}, function(err, foundUser){
     if(err){
@@ -56,7 +52,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req,res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
